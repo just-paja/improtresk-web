777e0966aa1fb14f62f6daa647b74319f4c9abc5
just-paja <just.paja@gmail.com>
2016-12-21 13:31:33 +0100

    Remove leading zeroes from human readable dates

--- src/web/constants/date.js
+++ src/web/constants/date.js
@@ -1,3 +1,3 @@
-export const FORMAT_HUMAN_DATE = 'DD. MM. YYYY';
-export const FORMAT_HUMAN_DATE_NO_YEAR = 'DD. MM.';
+export const FORMAT_HUMAN_DATE = 'D. M. YYYY';
+export const FORMAT_HUMAN_DATE_NO_YEAR = 'D. M.';
 export const FORMAT_HUMAN_TIME = 'HH:mm';
