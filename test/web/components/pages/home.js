76bd904874a2365520c5e75e8c85e30797a5349f
just-paja <just.paja@gmail.com>
2016-12-21 06:18:38 +0100

    Static content for home page

--- test/web/components/pages/home.js
+++ test/web/components/pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import sinon from 'sinon';
 
+import { Col, Row } from 'react-bootstrap';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
@@ -34,12 +35,25 @@ describe('Home component', () => {
       </Home>
     ).node).to.eql(
       <div>
-        <h1>Improtřesk</h1>
-        <News
-          news={[
-            { id: 1, text: 'foo', createdAt: '2016-01-02T03:04:05' },
-          ]}
-        />
+        <Row>
+          <Col md={6}>
+            <h2>O Improtřesku</h2>
+            <p>
+              Improtřesk je český festival divadelní improvizace a největší setkání
+              improvizátorů z celé České republiky. Každý rok se na Improtřesku
+              otevírají dílny z oblasti improvizačního divadla na kterých se schází nadšenci
+              improvizačního divadla i veřejnost.
+            </p>
+          </Col>
+          <Col md={6}>
+            <h2>Novinky</h2>
+            <News
+              news={[
+                { id: 1, text: 'foo', createdAt: '2016-01-02T03:04:05' },
+              ]}
+            />
+          </Col>
+        </Row>
       </div>
     );
   });
