76bd2ce48e905b94de7aa02b31991db5d61f267a
just-paja <just.paja@gmail.com>
2016-12-25 23:37:01 +0100

    Cover pages with tests

---
+++ test/web/components/pages/workshops.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import sinon from 'sinon';
+
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import Workshops from '../../../../src/web/components/pages/workshops';
+import WorkshopList from '../../../../src/web/components/workshopList';
+
+describe('Workshops page component', () => {
+  it('empty when not ready', () => {
+    expect(shallow(
+      <Workshops
+        onMount={() => {}}
+        workshops={[]}
+      />
+    ).node).to.equal(null);
+  });
+
+  it('renders content', () => {
+    expect(shallow(
+      <Workshops
+        onMount={() => {}}
+        routeParams={{}}
+        workshops={[
+          {
+            id: 12,
+            desc: 'foo',
+          },
+          {
+            id: 15,
+            desc: 'bar',
+          },
+        ]}
+        ready
+      />
+    ).node).to.eql(
+      <div>
+        <h1>Workshopy</h1>
+        <WorkshopList
+          workshops={[
+            {
+              id: 12,
+              desc: 'foo',
+            },
+            {
+              id: 15,
+              desc: 'bar',
+            },
+          ]}
+        />
+      </div>
+    );
+  });
+
+  it('calls onMount on componentWillMount', () => {
+    const mountSpy = sinon.spy();
+    const comp = shallow(
+      <Workshops
+        onMount={mountSpy}
+        routeParams={{
+          slug: 'foo',
+        }}
+        news={[]}
+      />
+    );
+
+    comp.instance().componentWillMount();
+    expect(mountSpy.calledTwice).to.equal(true);
+  });
+});
