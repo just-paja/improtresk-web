33cc5131663af96c2edd5e4c124a4033c4b506d7
just-paja <just.paja@gmail.com>
2016-12-25 20:08:11 +0100

    Show not current years in archive

--- src/web/selectors/years.js
+++ src/web/selectors/years.js
@@ -4,6 +4,11 @@ const getYearsState = state => state.years;
 
 export const yearsAll = createSelector(getYearsState, years => years.data);
 
+export const yearsNotCurrent = createSelector(
+  getYearsState,
+  years => years.data.filter(year => !year.current)
+);
+
 export const yearCurrent = createSelector(
   getYearsState,
   years => years.data.find(item => item.current) || null
