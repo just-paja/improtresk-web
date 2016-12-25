f831aed08cc2ca8e01f036d73613fc7763d511b9
Pavel Žák <just.paja@gmail.com>
2016-12-26 00:16:53 +0100

    Nock the static server requests

--- test/server/static.js
+++ test/server/static.js
@@ -1,8 +1,12 @@
+import nock from 'nock';
+
 import { expect, request } from 'chai';
 
 import runServer from '../../src/server';
 
 process.env.NODE_ENV = 'test';
+
+const apiEndpoint = 'https://private-6502a9-improtreskapi.apiary-mock.com:443/api';
 
 describe('Server', () => {
   let app;
@@ -19,13 +23,22 @@ describe('Server', () => {
     app.server.close();
   });
 
-  it('returns home title when requesting /', () =>
-    request(app)
+  it('returns home title when requesting /', () => {
+    nock(apiEndpoint)
+      .get('/years')
+      .reply(200, []);
+
+    nock(apiEndpoint)
+      .get('/news')
+      .reply(200, []);
+
+    return request(app)
       .get('/')
       .then((res) => {
+        expect(nock.isDone()).to.equal(true);
         expect(res).to.have.status(200);
         expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
         expect(res.text).to.contain('title');
-      })
-  );
+      });
+  });
 });
