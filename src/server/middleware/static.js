28f1fe73449375517238458cc601bec35b932079
just-paja <just.paja@gmail.com>
2016-12-27 14:38:35 +0100

    Fix path to fonts

--- src/server/middleware/static.js
+++ src/server/middleware/static.js
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 
 app.use('/font-awesome', express.static('node_modules/font-awesome'));
-app.use('/bootswatch', express.static('node_modules/bootswatch/sandstone'));
+app.use('/bootswatch', express.static('node_modules/bootswatch'));
 
 export default app;
