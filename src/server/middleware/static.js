aec8e992d6774609b0852a00d2518413dee08ee3
just-paja <just.paja@gmail.com>
2016-12-27 22:15:03 +0100

    Move static files out of source

--- src/server/middleware/static.js
+++ src/server/middleware/static.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.use('/theme', express.static('src/static'));
+app.use('/theme', express.static('static'));
 app.use('/font-awesome', express.static('node_modules/font-awesome'));
 app.use('/bootswatch', express.static('node_modules/bootswatch'));
 
