27d85fc83ad2dbedb144e92c078c75b9e4f68440
just-paja <just.paja@gmail.com>
2016-12-30 19:21:53 +0100

    Server, pass API source in errors

--- src/server/middleware/errorRender.jsx
+++ src/server/middleware/errorRender.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import winston from 'winston';
 
-import { renderToString } from 'react-dom/server';
-
 import NotFound from '../../web/components/notFound';
 import GeneralError from '../../web/components/generalError';
 
-import { renderInHtml } from './renderReact';
+import {
+  getStore,
+  renderMarkupAndWait,
+  respondWithHtml,
+} from './renderReact';
 
 const templateMap = {
   404: NotFound,
@@ -14,22 +16,23 @@ const templateMap = {
 
 const getTemplate = status => templateMap[status] || GeneralError;
 
-export default (req, res, next) => {
+export const renderAndRespond = (req, res, ErrorComponent) => {
+  const store = getStore(req);
+  const componentTree = <ErrorComponent />;
+
+  return renderMarkupAndWait(req, store, componentTree)
+    .then(markupAndState => respondWithHtml(req, res, markupAndState));
+};
+
+export default (req, res) => {
   if (res.statusCode === 200) {
     res.status(404);
   }
 
-  const ErrorComponent = getTemplate(res.statusCode);
-
   try {
-    const html = renderInHtml({
-      markup: renderToString(<ErrorComponent />),
-    });
-    res.send(html);
+    return renderAndRespond(req, res, getTemplate(res.statusCode));
   } catch (e) {
     winston.log('error', e);
     return res.status(500).send('Internal server errror');
   }
-
-  return next();
 };
