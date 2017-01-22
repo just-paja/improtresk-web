bc47b1453ddafd793d5115b58e59a1a0bb8d0e65
Pavel Žák <just.paja@gmail.com>
2017-01-22 18:04:14 +0100

    Separate home menu from page

--- test/web/components/pages/home.js
+++ test/web/components/pages/home.js
@@ -1,15 +1,11 @@
-import Markdown from 'react-markdown';
 import React from 'react';
 import sinon from 'sinon';
 
-import { Col, Row } from 'react-bootstrap';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import Container from '../../../../src/web/components/container';
 import Home from '../../../../src/web/components/pages/home';
-import Link from '../../../../src/web/components/link';
-import News from '../../../../src/web/components/news';
+import HomeMenu from '../../../../src/web/components/homeMenu';
 import YearDetail from '../../../../src/web/components/yearDetail';
 
 describe('Home page component', () => {
@@ -38,7 +34,46 @@ describe('Home page component', () => {
     ).node).to.equal(null);
   });
 
-  it('renders content', () => {
+  it('renders content for current year', () => {
+    expect(shallow(
+      <Home
+        about="bla bla bla"
+        onMount={() => {}}
+        news={[
+          { id: 1, text: 'foo', createdAt: '2016-01-02T03:04:05' },
+        ]}
+        ready
+        year={{
+          current: true,
+          startAt: '2016-01-02',
+          endAt: '2016-01-05',
+          startSignupsAt: '2016-01-01T00:00:00',
+          topic: 'foo',
+          year: '2016',
+        }}
+      >
+        <div>foo</div>
+      </Home>
+    ).node).to.eql(
+      <div className="year-2016">
+        <YearDetail
+          endAt="2016-01-05"
+          startAt="2016-01-02"
+          startSignupsAt="2016-01-01T00:00:00"
+          topic="foo"
+          year="2016"
+        />
+        <HomeMenu
+          about="bla bla bla"
+          news={[
+            { id: 1, text: 'foo', createdAt: '2016-01-02T03:04:05' },
+          ]}
+        />
+      </div>
+    );
+  });
+
+  it('renders content for next year', () => {
     expect(shallow(
       <Home
         about="bla bla bla"
@@ -67,30 +102,6 @@ describe('Home page component', () => {
           topic="foo"
           year="2016"
         />
-        <Container>
-          <Row>
-            <Col md={6}>
-              <h2>O Improtřesku</h2>
-              <Markdown source="bla bla bla" />
-
-              <h3>Rychlé odkazy</h3>
-              <ul className="list-unstyled">
-                <li><Link to="location"><b>Kde</b> to je?</Link></li>
-                <li><Link to="fees"><b>Kolik</b> to stojí?</Link></li>
-                <li><Link to="accomodation">Jak je to se <b>spaní</b>m?</Link></li>
-                <li><Link to="workshops">Jaké jsou <b>workshop</b>y?</Link></li>
-              </ul>
-            </Col>
-            <Col md={6}>
-              <h2>Novinky</h2>
-              <News
-                news={[
-                  { id: 1, text: 'foo', createdAt: '2016-01-02T03:04:05' },
-                ]}
-              />
-            </Col>
-          </Row>
-        </Container>
       </div>
     );
   });
