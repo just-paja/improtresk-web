e8ca4a044e2e6841f27c00a9b06854104eea8670
Pavel Žák <just.paja@gmail.com>
2017-02-10 18:23:32 +0100

    Food background

--- src/web/components/pages/food.jsx
+++ src/web/components/pages/food.jsx
@@ -1,6 +1,9 @@
+import Col from 'react-bootstrap/lib/Col';
 import Helmet from 'react-helmet';
+import Image from 'react-bootstrap/lib/Image';
 import Markdown from 'react-markdown';
 import React, { Component, PropTypes } from 'react';
+import Row from 'react-bootstrap/lib/Row';
 
 import Container from '../container';
 import FoodTime from '../foodTime';
@@ -27,14 +30,21 @@ export default class Food extends Component {
           ]}
         />
         <h1>{title}</h1>
-        <Markdown source={intro} />
-        <ul>
-          {foodTimes.map(foodTime =>
-            <li key={foodTime.id}>
-              <FoodTime {...foodTime} />
-            </li>
-          )}
-        </ul>
+        <Row>
+          <Col md={6}>
+            <Markdown source={intro} />
+            <ul>
+              {foodTimes.map(foodTime =>
+                <li key={foodTime.id}>
+                  <FoodTime {...foodTime} />
+                </li>
+              )}
+            </ul>
+          </Col>
+          <Col md={6}>
+            <Image responsive src="/static/theme/2017/bg-food.jpg" />
+          </Col>
+        </Row>
       </Container>
     );
   }
