312a2a4378635c948488f523b1d027ad2192a94f
just-paja <just.paja@gmail.com>
2017-02-12 23:26:13 +0100

    Add trailing slashes to the API

--- src/web/api.js
+++ src/web/api.js
@@ -6,29 +6,29 @@ const apiFetch = (url, { apiSource, ...params }) =>
 export const fetchArchivedYear = ({ year, ...params }) =>
   apiFetch(`years/${year}`, params);
 
-export const fetchAccomodation = params => apiFetch('accomodations', params);
+export const fetchAccomodation = params => apiFetch('accomodations/', params);
 
-export const fetchConditionsCurrent = params => apiFetch('conditions', params);
+export const fetchConditionsCurrent = params => apiFetch('conditions/', params);
 
 export const fetchLectorRoles = params => apiFetch('lectorRoles/', params);
 
 export const fetchLectors = params => apiFetch('lectors/', params);
 
-export const fetchMeals = params => apiFetch('meals', params);
+export const fetchMeals = params => apiFetch('meals/', params);
 
-export const fetchNews = params => apiFetch('news', params);
+export const fetchNews = params => apiFetch('news/', params);
 
 export const fetchNewsDetail = ({ news, ...params }) =>
-  apiFetch(`news/${news}`, params);
+  apiFetch(`news/${news}/`, params);
 
 export const fetchText = ({ code, ...params }) => apiFetch(`texts/${code}`, params);
 
-export const fetchTips = params => apiFetch('tips', params);
+export const fetchTips = params => apiFetch('tips/', params);
 
-export const fetchYears = params => apiFetch('years', params);
+export const fetchYears = params => apiFetch('years/', params);
 
 export const fetchWorkshopDetail = ({ workshop, year, ...params }) =>
-  apiFetch(`years/${year}/workshops/${workshop}`, params);
+  apiFetch(`years/${year}/workshops/${workshop}/`, params);
 
 export const fetchWorkshopDifficulties = ({ ...params }) =>
   apiFetch('workshopDifficulties/', params);
@@ -36,7 +36,7 @@ export const fetchWorkshopDifficulties = ({ ...params }) =>
 export const fetchWorkshops = ({ year, ...params }) =>
   apiFetch(`years/${year}/workshops/`, params);
 
-export const signup = ({ data, ...params }) => apiFetch('signup', {
+export const signup = ({ data, ...params }) => apiFetch('signup/', {
   ...params,
   method: 'POST',
   body: JSON.stringify(data),
