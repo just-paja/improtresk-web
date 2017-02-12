f810d58508f137d11a58b160a9de1cfb30e044b0
just-paja <just.paja@gmail.com>
2017-02-12 23:49:17 +0100

    Trust the proxy

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,7 @@
 const defaultConfig = {
   logLevel: process.env.NODE_LOG_LEVEL || 'info',
   port: process.env.NODE_PORT || 3000,
+  proxy: process.env.NODE_AS_PROXY || false,
   apiSource: process.env.NODE_API_URL ||
     'https://private-6502a9-improtreskapi.apiary-mock.com/api',
 };
