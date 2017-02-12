f85f53c3341dfb18c5d778c2bc9979fa14c6667e
just-paja <just.paja@gmail.com>
2017-02-12 22:21:01 +0100

    News detail, render photos

--- src/web/components/pages/newsDetail.jsx
+++ src/web/components/pages/newsDetail.jsx
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/lib/Row';
 import React, { Component, PropTypes } from 'react';
 
 import Container from '../container';
+import Gallery from '../gallery';
 import News from '../news';
 import NotFound from '../notFound';
 
@@ -39,14 +40,34 @@ export default class NewsDetail extends Component {
           meta={[
             {
               property: 'og:name',
+              content: newsDetail.name,
+            },
+            {
+              property: 'og:description',
               content: `${newsDetail.text.substr(0, 127)}...`,
             },
+            ...newsDetail.photos.reduce((data, photo) => ([
+              ...data,
+              {
+                property: 'og:image:url',
+                content: photo.image,
+              },
+              {
+                property: 'og:image:height',
+                content: photo.height,
+              },
+              {
+                property: 'og:image:width',
+                content: photo.width,
+              },
+            ]), []),
           ]}
         />
         <Row>
           <Col md={8}>
             <h1>{newsDetail.name}</h1>
             <Markdown source={newsDetail.text} />
+            <Gallery photos={newsDetail.photos} />
           </Col>
           <Col md={4}>
             <h2>Ostatní novinky</h2>
