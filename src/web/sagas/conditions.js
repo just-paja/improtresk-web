acfeda1ca6e7e0b0a20f8001fd151893856ff9a1
Pavel Žák <just.paja@gmail.com>
2017-02-13 02:06:56 +0100

    Add conditions endpoint

--- src/web/sagas/conditions.js
+++ src/web/sagas/conditions.js
@@ -1,16 +1,15 @@
-import { takeLatest } from 'redux-saga/effects';
+import { fork, select, takeLatest } from 'redux-saga/effects';
 
 import { fetchResourceIfNeeded } from './common';
 import { isValid } from '../selectors/conditions';
+import { yearActiveNumber } from '../selectors/years';
 
 import * as api from '../api';
 import * as constants from '../constants/actions';
 
-export function* fetchCurrentConditionsOnMount() {
-  yield takeLatest(
-    [
-      constants.CONDITIONS_MOUNTED,
-    ],
+export function* fetchYearsConditions() {
+  const year = yield select(yearActiveNumber);
+  yield fork(
     fetchResourceIfNeeded,
     api.fetchConditionsCurrent,
     isValid,
@@ -18,10 +17,17 @@ export function* fetchCurrentConditionsOnMount() {
       onStart: constants.CONDITIONS_CURRENT_FETCH_STARTED,
       onSuccess: constants.CONDITIONS_CURRENT_FETCH_SUCCESS,
       onError: constants.CONDITIONS_CURRENT_FETCH_ERROR,
+      year,
     }
+  );
+}
+export function* fetchConditions() {
+  yield takeLatest(
+    constants.CONDITIONS_MOUNTED,
+    fetchYearsConditions
   );
 }
 
 export default [
-  fetchCurrentConditionsOnMount,
+  fetchConditions,
 ];
