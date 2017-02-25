3985218a4a8dc8f0efa40573426a2b28b581e9eb
Pavel Žák <just.paja@gmail.com>
2017-02-25 12:29:01 +0100

    Add workshop price selector

--- test/web/selectors/workshops.js
+++ test/web/selectors/workshops.js
@@ -27,15 +27,95 @@ describe('Workshops selectors', () => {
             {
               id: 1,
               lectors: [],
+              prices: [],
             },
           ],
         },
+      },
+      years: {
+        data: [],
       },
     })).to.eql([
       {
         id: 1,
         difficulty: null,
         lectors: [],
+        prices: [],
+      },
+    ]);
+  });
+  it('workshopsAll returns all workshops stored with price levels mapped', () => {
+    expect(workshopsAll({
+      lectors: {
+        list: {
+          data: [],
+        },
+        roles: {
+          data: [],
+        },
+      },
+      workshops: {
+        difficulties: {
+          data: [],
+        },
+        list: {
+          data: [
+            {
+              id: 1,
+              lectors: [],
+              prices: [
+                {
+                  price_level: 1,
+                  price: 200,
+                },
+                {
+                  price_level: 2,
+                  price: 400,
+                },
+              ],
+            },
+          ],
+        },
+      },
+      years: {
+        data: [
+          {
+            year: '2016',
+            current: true,
+            priceLevels: [
+              {
+                id: 1,
+                name: 'Zlevněná',
+                takesEffectOn: '2016-01-02T03:04:05Z',
+              },
+              {
+                id: 2,
+                name: 'Základní',
+                takesEffectOn: '2016-03-02T03:04:05Z',
+              },
+            ],
+          },
+        ],
+      },
+    })).to.eql([
+      {
+        id: 1,
+        difficulty: null,
+        lectors: [],
+        prices: [
+          {
+            endsOn: '2016-03-02T03:04:05Z',
+            level: 'Zlevněná',
+            price: 200,
+            takesEffectOn: '2016-01-02T03:04:05Z',
+          },
+          {
+            endsOn: null,
+            level: 'Základní',
+            price: 400,
+            takesEffectOn: '2016-03-02T03:04:05Z',
+          },
+        ],
       },
     ]);
   });
@@ -55,17 +135,22 @@ describe('Workshops selectors', () => {
             id: 1,
             name: 'foo',
             lectors: [],
+            prices: [],
           },
         },
         difficulties: {
           data: [],
         },
       },
+      years: {
+        data: [],
+      },
     })).to.eql({
       id: 1,
       difficulty: null,
       name: 'foo',
       lectors: [],
+      prices: [],
     });
   });
   it('shouldFetchList returns false when in invalid state', () => {
