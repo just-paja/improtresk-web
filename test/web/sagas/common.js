287e2ae4a4e59c5984b97f0d1f22e525b2a15249
Pavel Žák <just.paja@gmail.com>
2017-03-12 13:31:02 +0100

    Cover signup and common sagas

--- test/web/sagas/common.js
+++ test/web/sagas/common.js
@@ -23,7 +23,6 @@ describe('Common saga helpers', () => {
     expect(testFetchIfNeeded.next().value).to.eql(select(selector));
     expect(testFetchIfNeeded.next(true).done).to.equal(true);
   });
-
   it('fetchResourceIfNeeded calls fetchResource when validity selector returns false', () => {
     const selector = () => false;
     const fetch = () => {};
@@ -53,7 +52,6 @@ describe('Common saga helpers', () => {
       { type: 'REDUX_ACTION' }
     ));
   });
-
   it('fetchResource dispatches onStart and onSuccess actions', () => {
     const fetch = () => {};
     const testFetch = fetchResource(
@@ -92,7 +90,110 @@ describe('Common saga helpers', () => {
       },
     }));
   });
+  it('fetchResource dispatches onStart and onSuccess 204 actions', () => {
+    const fetch = () => {};
+    const testFetch = fetchResource(
+      fetch,
+      {
+        onStart: 'started',
+        onSuccess: 'succeeded',
+        onError: 'failed',
+        code: 'code',
+      },
+      { type: 'REDUX_ACTION' }
+    );
 
+    expect(testFetch.next().value).to.eql(select(getApiSource));
+    expect(testFetch.next('http://localhost').value)
+      .to.eql(put({ type: 'started', code: 'code' }));
+    expect(testFetch.next().value)
+      .to.eql(call(fetch, {
+        apiSource: 'http://localhost',
+        code: 'code',
+        type: 'REDUX_ACTION',
+      }));
+
+    const response = { status: 204 };
+
+    expect(testFetch.next(response).value).to.eql([]);
+    expect(testFetch.next([]).value).to.eql(put({
+      type: 'succeeded',
+      code: 'code',
+      data: [],
+    }));
+  });
+  it('fetchResource dispatches onStart and onError actions on < 200 status', () => {
+    const fetch = () => {};
+    const testFetch = fetchResource(
+      fetch,
+      {
+        onStart: 'started',
+        onSuccess: 'succeeded',
+        onError: 'failed',
+        code: 'code',
+      },
+      { type: 'REDUX_ACTION' }
+    );
+
+    expect(testFetch.next().value).to.eql(select(getApiSource));
+    expect(testFetch.next('http://localhost').value)
+      .to.eql(put({ type: 'started', code: 'code' }));
+    expect(testFetch.next().value)
+      .to.eql(call(fetch, {
+        apiSource: 'http://localhost',
+        code: 'code',
+        type: 'REDUX_ACTION',
+      }));
+    expect(testFetch.next({
+      status: 199,
+      json: () => ({ errors: ['test error'] }),
+      error: 'foo',
+    }).value).to.eql({ errors: ['test error'] });
+    expect(testFetch.next({ errors: ['test error'] }).value).to.eql(put({
+      type: 'failed',
+      data: { errors: ['test error'] },
+      code: 'code',
+      status: 199,
+      error: 'foo',
+    }));
+    expect(testFetch.next().done).to.equal(true);
+  });
+  it('fetchResource dispatches onStart and onError actions on < 400 status', () => {
+    const fetch = () => {};
+    const testFetch = fetchResource(
+      fetch,
+      {
+        onStart: 'started',
+        onSuccess: 'succeeded',
+        onError: 'failed',
+        code: 'code',
+      },
+      { type: 'REDUX_ACTION' }
+    );
+
+    expect(testFetch.next().value).to.eql(select(getApiSource));
+    expect(testFetch.next('http://localhost').value)
+      .to.eql(put({ type: 'started', code: 'code' }));
+    expect(testFetch.next().value)
+      .to.eql(call(fetch, {
+        apiSource: 'http://localhost',
+        code: 'code',
+        type: 'REDUX_ACTION',
+      }));
+    expect(testFetch.next({
+      status: 400,
+      json: () => ({ errors: ['test error'] }),
+      error: 'foo',
+    }).value).to.eql({ errors: ['test error'] });
+    expect(testFetch.next({ errors: ['test error'] }).value).to.eql(put({
+      type: 'failed',
+      data: { errors: ['test error'] },
+      code: 'code',
+      status: 400,
+      error: 'foo',
+    }));
+    expect(testFetch.next().done).to.equal(true);
+  });
   it('fetchResource dispatches onStart and onError actions', () => {
     const fetch = () => {};
     const testFetch = fetchResource(
--- test/web/sagas/signups.js
+++ test/web/sagas/signups.js
@@ -1,13 +1,33 @@
 import { expect } from 'chai';
-import { fork, select } from 'redux-saga/effects';
+import { fork, put, select, takeLatest } from 'redux-saga/effects';
+import { push } from 'react-router-redux';
 
 import { getForm } from '../../../src/web/selectors/forms';
 import { sendForm } from '../../../src/web/sagas/forms';
-import { sendLogin, sendSignup } from '../../../src/web/sagas/signup';
+import {
+  loginOnFormSubmit,
+  loginOnSignup,
+  loginSignup,
+  selectLoginSubmit,
+  selectSignupSubmit,
+  selectSignupSuccess,
+  sendLogin,
+  sendSignup,
+  signupOnFormSubmit,
+} from '../../../src/web/sagas/signup';
 
 import * as api from '../../../src/web/api';
 
 describe('Signup sagas', () => {
+  it('selectSignupSubmit selects signup submit action', () => {
+    expect(selectSignupSubmit({ type: 'FORM_SUBMIT_ALLOWED', form: 'signup' })).to.equal(true);
+  });
+  it('selectSignupSuccess selects signup submit action', () => {
+    expect(selectSignupSuccess({ type: 'FORM_SUBMIT_SUCCESS', form: 'signup' })).to.equal(true);
+  });
+  it('selectLoginSubmit selects signup submit action', () => {
+    expect(selectLoginSubmit({ type: 'FORM_SUBMIT_ALLOWED', form: 'login' })).to.equal(true);
+  });
   it('sendSignup creates form submit actions', () => {
     const saga = sendSignup({ type: 'REDUX_ACTION', form: 'signup' });
     expect(saga.next().value).to.eql(select(getForm, 'signup'));
@@ -34,6 +54,39 @@ describe('Signup sagas', () => {
       email: 'mail@test.com',
       password: 'foo',
     }));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginSignup creates actions', () => {
+    const saga = loginSignup({
+      type: 'REDUX_ACTION',
+      data: {
+        id: 1,
+        name: 'Hugo Ventil',
+      },
+    });
+    expect(saga.next().value).to.eql(put({
+      type: 'SIGNUP_REGISTERED',
+      data: {
+        id: 1,
+        name: 'Hugo Ventil',
+      },
+    }));
+    expect(saga.next().value).to.eql(put(push('/ucastnik')));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('signupOnFormSubmit creates actions', () => {
+    const saga = signupOnFormSubmit();
+    expect(saga.next().value).to.eql(takeLatest(selectSignupSubmit, sendSignup));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginOnSignup creates actions', () => {
+    const saga = loginOnSignup();
+    expect(saga.next().value).to.eql(takeLatest(selectSignupSuccess, loginSignup));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginOnFormSubmit creates actions', () => {
+    const saga = loginOnFormSubmit();
+    expect(saga.next().value).to.eql(takeLatest(selectLoginSubmit, sendLogin));
     expect(saga.next().done).to.equal(true);
   });
 });
