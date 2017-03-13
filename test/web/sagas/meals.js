49039188d5f406b2a96c792b5b90c71b6f44d100
Pavel Žák <just.paja@gmail.com>
2017-03-13 20:15:51 +0100

    Cover Meals sagas

---
+++ test/web/sagas/meals.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import { takeLatest } from 'redux-saga/effects';
+
+import { fetchResourceIfNeeded } from '../../../src/web/sagas/common';
+import { fetchTextsIfNeeded } from '../../../src/web/sagas/texts';
+import {
+  fetchMealsOnMount,
+  fetchTextsOnMount,
+} from '../../../src/web/sagas/meals';
+import { isValid } from '../../../src/web/selectors/food';
+
+import * as api from '../../../src/web/api';
+
+describe('Meals sagas', () => {
+  it('fetchMealsOnMount creates fetch actions', () => {
+    const saga = fetchMealsOnMount();
+    expect(saga.next().value).to.eql(takeLatest(
+      'FOOD_MOUNTED',
+      fetchResourceIfNeeded,
+      api.fetchMeals,
+      isValid,
+      {
+        onStart: 'MEALS_FETCH_STARTED',
+        onSuccess: 'MEALS_FETCH_SUCCESS',
+        onError: 'MEALS_FETCH_ERROR',
+      }
+    ));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('fetchTextsOnMount creates fetch actions', () => {
+    const saga = fetchTextsOnMount();
+    expect(saga.next().value).to.eql(takeLatest(
+      'FOOD_MOUNTED',
+      fetchTextsIfNeeded,
+      ['food-intro']
+    ));
+    expect(saga.next().done).to.equal(true);
+  });
+});
