7278b1b12bf2caba6748ede2dc6442003588c140
just-paja <just.paja@gmail.com>
2017-03-13 19:42:52 +0100

    Cover Prop component

--- test/web/components/prop.js
+++ test/web/components/prop.js
@@ -7,7 +7,7 @@ import { shallow } from 'enzyme';
 import Prop from '../../../src/web/components/prop';
 
 describe('Prop component', () => {
-  it('renders price', () => {
+  it('renders', () => {
     expect(shallow(
       <Prop label="Prop Label" icon="Prop Icon">foo</Prop>
     ).node).to.eql(
@@ -15,7 +15,21 @@ describe('Prop component', () => {
         <FontAwesome name="Prop Icon" /> Prop Label:{' '}
         foo
       </li>
-
     );
+  });
+  it('renders without icon', () => {
+    expect(shallow(
+      <Prop label="Prop Label">foo</Prop>
+    ).node).to.eql(
+      <li>
+        Prop Label:{' '}
+        foo
+      </li>
+    );
+  });
+  it('renders empty without children', () => {
+    expect(shallow(
+      <Prop label="Prop Label" />
+    ).node).to.equal(null);
   });
 });
