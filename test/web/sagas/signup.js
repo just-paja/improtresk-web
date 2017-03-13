aad6681738becce62481001e60ad701a6a3b2686
Pavel Žák <just.paja@gmail.com>
2017-03-13 19:58:22 +0100

    Cover Signups sagas

--- test/web/sagas/signup.js
+++ test/web/sagas/signup.js
@@ -1,15 +1,26 @@
+import cookie from 'js-cookie';
+import sinon from 'sinon';
+
 import { expect } from 'chai';
 import { fork, put, select, takeLatest } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
 
 import { getForm } from '../../../src/web/selectors/forms';
+import {
+  getApiAuth,
+  getAutoLoginStatus,
+} from '../../../src/web/selectors/session';
 import { sendForm } from '../../../src/web/sagas/forms';
 import {
   login,
   loginOnFormSubmit,
+  loginOnAction,
+  loginOnMount,
   loginOnSignup,
   loginSignup,
+  loginWithCookie,
   selectLoginSubmit,
+  selectLoginSuccess,
   selectSignupSubmit,
   selectSignupSuccess,
   sendLogin,
@@ -20,6 +31,12 @@ import {
 import * as api from '../../../src/web/api';
 
 describe('Signup sagas', () => {
+  beforeEach(() => {
+    sinon.stub(cookie, 'getJSON');
+  });
+  afterEach(() => {
+    cookie.getJSON.restore();
+  });
   it('selectSignupSubmit selects signup submit action', () => {
     expect(selectSignupSubmit({ type: 'FORM_SUBMIT_ALLOWED', form: 'signup' })).to.equal(true);
   });
@@ -28,6 +45,9 @@ describe('Signup sagas', () => {
   });
   it('selectLoginSubmit selects signup submit action', () => {
     expect(selectLoginSubmit({ type: 'FORM_SUBMIT_ALLOWED', form: 'login' })).to.equal(true);
+  });
+  it('selectLoginSuccess selects signup submit action', () => {
+    expect(selectLoginSuccess({ type: 'FORM_SUBMIT_SUCCESS', form: 'login' })).to.equal(true);
   });
   it('sendSignup creates form submit actions', () => {
     const saga = sendSignup({ type: 'REDUX_ACTION', form: 'signup' });
@@ -45,6 +65,21 @@ describe('Signup sagas', () => {
       email: 'mail@test.com',
       password: 'foo',
       team_name: 'Test Team',
+    }));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('sendSignup creates form submit actions without team name', () => {
+    const saga = sendSignup({ type: 'REDUX_ACTION', form: 'signup' });
+    expect(saga.next().value).to.eql(select(getForm, 'signup'));
+    expect(saga.next({
+      values: {
+        email: 'mail@test.com',
+        password: 'foo',
+      },
+    }).value).to.eql(fork(sendForm, api.signup, 'signup', {
+      email: 'mail@test.com',
+      password: 'foo',
+      team_name: null,
     }));
     expect(saga.next().done).to.equal(true);
   });
@@ -96,14 +131,62 @@ describe('Signup sagas', () => {
     expect(saga.next().value).to.eql(takeLatest(selectSignupSubmit, sendSignup));
     expect(saga.next().done).to.equal(true);
   });
-  it('loginOnSignup creates actions', () => {
+  it('loginOnSignup creates login actions', () => {
     const saga = loginOnSignup();
     expect(saga.next().value).to.eql(takeLatest(selectSignupSuccess, loginSignup));
     expect(saga.next().done).to.equal(true);
   });
-  it('loginOnFormSubmit creates actions', () => {
+  it('loginOnAction creates login actions', () => {
+    const saga = loginOnAction();
+    expect(saga.next().value).to.eql(takeLatest(selectLoginSuccess, login));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginOnMount creates login actions', () => {
+    const saga = loginOnMount();
+    expect(saga.next().value).to.eql(takeLatest('APP_MOUNTED', loginWithCookie));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginOnFormSubmit creates login actions', () => {
     const saga = loginOnFormSubmit();
     expect(saga.next().value).to.eql(takeLatest(selectLoginSubmit, sendLogin));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginOnWithCookie creates no action when already attempted', () => {
+    const saga = loginWithCookie();
+    expect(saga.next().value).to.eql(select(getAutoLoginStatus));
+    expect(saga.next(true).done).to.equal(true);
+  });
+  it('loginOnWithCookie creates login action with auth data', () => {
+    const saga = loginWithCookie();
+    expect(saga.next().value).to.eql(select(getAutoLoginStatus));
+    expect(saga.next(false).value).to.eql(select(getApiAuth));
+    expect(saga.next({
+      access_token: 'foo',
+    }).value).to.eql(put({
+      type: 'PARTICIPANT_LOGIN',
+      data: { access_token: 'foo' },
+    }));
+    expect(saga.next().value).to.eql(put({ type: 'PARTICIPANT_LOGIN_AUTO' }));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginOnWithCookie creates login action with cookie auth data', () => {
+    cookie.getJSON.returns({ access_token: 'foo' });
+    const saga = loginWithCookie();
+    expect(saga.next().value).to.eql(select(getAutoLoginStatus));
+    expect(saga.next(false).value).to.eql(select(getApiAuth));
+    expect(saga.next({}).value).to.eql(put({
+      type: 'PARTICIPANT_LOGIN',
+      data: { access_token: 'foo' },
+    }));
+    expect(saga.next().value).to.eql(put({ type: 'PARTICIPANT_LOGIN_AUTO' }));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('loginOnWithCookie creates no login action with empty cookie auth data', () => {
+    cookie.getJSON.returns(null);
+    const saga = loginWithCookie();
+    expect(saga.next().value).to.eql(select(getAutoLoginStatus));
+    expect(saga.next(false).value).to.eql(select(getApiAuth));
+    expect(saga.next({}).value).to.eql(put({ type: 'PARTICIPANT_LOGIN_AUTO' }));
     expect(saga.next().done).to.equal(true);
   });
   it('login creates actions', () => {
