8078e424b8124addc752e9482fe65b3f8cf8402e
just-paja <just.paja@gmail.com>
2017-03-14 01:30:58 +0100

    Bring back image lightbox

--- src/web/components/gallery.jsx
+++ src/web/components/gallery.jsx
@@ -1,23 +1,65 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import Lightbox from 'react-images';
 import ReactGallery from 'react-photo-gallery';
 
-const Gallery = ({ photos, ...other }) => (
-  <ReactGallery
-    {...other}
-    photos={photos.map(photo => ({
-      ...photo,
+export default class Gallery extends Component {
+  constructor() {
+    super();
+
+    this.state = { lightbox: false };
+    this.handlePhotoClick = this.handlePhotoClick.bind(this);
+    this.handleLightboxClose = this.handleLightboxClose.bind(this);
+    this.prev = this.prev.bind(this);
+    this.next = this.next.bind(this);
+  }
+
+  handleLightboxClose() {
+    this.setState({ lightbox: false });
+  }
+
+  handlePhotoClick(index, e) {
+    e.preventDefault();
+    this.setState({ image: index, lightbox: true });
+  }
+
+  prev() {
+    this.setState({ image: this.state.image - 1 });
+  }
+
+  next() {
+    this.setState({ image: this.state.image + 1 });
+  }
+
+  render() {
+    const { photos, ...other } = this.props;
+    const photosMapped = photos.map(photo => ({
+      caption: photo.caption,
       src: photo.image,
+      height: photo.height,
+      width: photo.width,
       aspectRatio: photo.width / photo.height,
-      lightboxImage: {
-        src: photo.image,
-        caption: photo.caption,
-      },
-    }))}
-  />
-);
+    }));
+    return (
+      <div>
+        <ReactGallery
+          {...other}
+          onClickPhoto={this.handlePhotoClick}
+          photos={photosMapped}
+        />
+        <Lightbox
+          images={photosMapped}
+          backdropClosesModal
+          onClose={this.handleLightboxClose}
+          onClickPrev={this.prev}
+          onClickNext={this.next}
+          currentImage={this.state.image}
+          isOpen={this.state.lightbox}
+        />
+      </div>
+    );
+  }
+}
 
 Gallery.propTypes = {
   photos: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
-
-export default Gallery;
