b148b49115c3c0441326e55f08cb9ebcc56ef95d
just-paja <just.paja@gmail.com>
2017-03-16 03:35:55 +0100

    Integrate Google Analytics

--- src/web/components/root.jsx
+++ src/web/components/root.jsx
@@ -1,3 +1,4 @@
+import Analytics from 'react-ga';
 import React, { PropTypes } from 'react';
 
 import { Router } from 'react-router';
@@ -7,10 +8,16 @@ import 'bootswatch/sandstone/bootstrap.min.css';
 
 import configureRoutes from '../routes';
 
+Analytics.initialize('UA-40099806-8');
+
+const logPage = () => {
+  Analytics.set({ page: window.location.pathname });
+  Analytics.pageview(window.location.pathname);
+};
 
 const Root = ({ history, store }) => (
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={history} onUpdate={logPage}>
       {configureRoutes(store)}
     </Router>
   </Provider>
