cd41ff6edcf04d287f964793d577df0cf4ccc8f8
Pavel Žák <just.paja@gmail.com>
2017-03-30 20:07:17 +0200

    Bind reset password saga

--- test/web/sagas/participantPassword.js
+++ test/web/sagas/participantPassword.js
@@ -6,10 +6,13 @@ import { getForm } from '../../../src/web/selectors/forms';
 import {
   bindPasswordChangeSubmit,
   bindPasswordNewSubmit,
+  bindPasswordResetSubmit,
   passwordChange,
   passwordNew,
+  passwordReset,
   selectPasswordChangeSubmit,
   selectPasswordNewSubmit,
+  selectPasswordResetSubmit,
 } from '../../../src/web/sagas/participanPassword';
 
 import * as api from '../../../src/web/api';
@@ -28,6 +31,14 @@ describe('Participant sagas', () => {
     expect(saga.next().value).to.eql(takeLatest(
       selectPasswordChangeSubmit,
       passwordChange
+    ));
+    expect(saga.next().done).to.equal(true);
+  });
+  it('bindPasswordResetSubmit binds fetch actions', () => {
+    const saga = bindPasswordResetSubmit();
+    expect(saga.next().value).to.eql(takeLatest(
+      selectPasswordResetSubmit,
+      passwordReset
     ));
     expect(saga.next().done).to.equal(true);
   });
@@ -75,6 +86,27 @@ describe('Participant sagas', () => {
     ));
     expect(saga.next().done).to.equal(true);
   });
+  it('passwordReset creates select and fetch actions', () => {
+    const saga = passwordReset();
+    expect(saga.next().value).to.eql(select(getForm, 'resetPassword'));
+    expect(saga.next({
+      values: {
+        email: 'foo@bar.com',
+      },
+    }).value).to.eql(call(
+      fetchResource,
+      api.resetPassword,
+      {
+        onStart: 'PARTICIPANT_PASSWORD_RESET_STARTED',
+        onSuccess: 'PARTICIPANT_PASSWORD_RESET_SUCCESS',
+        onError: 'PARTICIPANT_PASSWORD_RESET_ERROR',
+        data: {
+          email: 'foo@bar.com',
+        },
+      }
+    ));
+    expect(saga.next().done).to.equal(true);
+  });
   it('selectPasswordNewSubmit selects new password form submit', () => {
     expect(selectPasswordNewSubmit({
       type: 'FORM_SUBMIT_ALLOWED',
@@ -85,6 +117,12 @@ describe('Participant sagas', () => {
     expect(selectPasswordChangeSubmit({
       type: 'FORM_SUBMIT_ALLOWED',
       form: 'changePassword',
+    })).to.equal(true);
+  });
+  it('selectPasswordResetSubmit selects new password form submit', () => {
+    expect(selectPasswordResetSubmit({
+      type: 'FORM_SUBMIT_ALLOWED',
+      form: 'resetPassword',
     })).to.equal(true);
   });
 });
