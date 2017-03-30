81f956055a5549d914a10e9afb3d08b5d33b7448
just-paja <just.paja@gmail.com>
2017-03-30 20:07:17 +0200

    Bind reset password saga

--- src/web/sagas/participanPassword.js
+++ src/web/sagas/participanPassword.js
@@ -6,11 +6,28 @@ import { getForm } from '../selectors/forms';
 import * as api from '../api';
 import * as constants from '../constants/actions';
 
+export const selectPasswordResetSubmit = action =>
+  action.type === constants.FORM_SUBMIT_ALLOWED && action.form === 'resetPassword';
+
 export const selectPasswordChangeSubmit = action =>
   action.type === constants.FORM_SUBMIT_ALLOWED && action.form === 'changePassword';
 
 export const selectPasswordNewSubmit = action =>
   action.type === constants.FORM_SUBMIT_ALLOWED && action.form === 'newPassword';
+
+export function* passwordReset() {
+  const password = yield select(getForm, 'resetPassword');
+  yield call(
+    fetchResource,
+    api.resetPassword,
+    {
+      onStart: constants.PARTICIPANT_PASSWORD_RESET_STARTED,
+      onSuccess: constants.PARTICIPANT_PASSWORD_RESET_SUCCESS,
+      onError: constants.PARTICIPANT_PASSWORD_RESET_ERROR,
+      data: password.values,
+    }
+  );
+}
 
 export function* passwordNew() {
   const password = yield select(getForm, 'newPassword');
@@ -40,6 +57,10 @@ export function* passwordChange() {
   );
 }
 
+export function* bindPasswordResetSubmit() {
+  yield takeLatest(selectPasswordResetSubmit, passwordReset);
+}
+
 export function* bindPasswordChangeSubmit() {
   yield takeLatest(selectPasswordChangeSubmit, passwordChange);
 }
@@ -51,4 +72,5 @@ export function* bindPasswordNewSubmit() {
 export default [
   bindPasswordChangeSubmit,
   bindPasswordNewSubmit,
+  bindPasswordResetSubmit,
 ];
