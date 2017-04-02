859ccc815e7734df9ca192e174639a860c469860
Pavel Žák <just.paja@gmail.com>
2017-04-02 19:56:35 +0200

    Simplify development config

--- src/server/config.js
+++ src/server/config.js
@@ -3,11 +3,13 @@ const defaultConfig = {
   port: process.env.NODE_PORT || 3000,
   proxy: process.env.NODE_AS_PROXY || false,
   apiSource: process.env.NODE_API_URL ||
-    'https://private-6502a9-improtreskapi.apiary-mock.com/api',
+    'http://localhost:8000/api',
   apiAuthSource: process.env.NODE_API_AUTH_URL ||
-    'private-6502a9-improtreskapi.apiary-mock.com/o/token',
-  apiId: process.env.NODE_API_CLIENT_ID,
-  apiSecret: process.env.NODE_API_CLIENT_SECRET,
+    'http://localhost:8000/o',
+  apiId: process.env.NODE_API_CLIENT_ID ||
+    'development-client-id',
+  apiSecret: process.env.NODE_API_CLIENT_SECRET ||
+    'development-client-secret',
 };
 
 export default (userConfig = {}) => ({ ...defaultConfig, ...userConfig });
