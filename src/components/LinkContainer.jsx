5e6607b2cc22bcf1aaa853a830fb10d1e5d3f47e
just-paja <just.paja@gmail.com>
2018-05-05 13:36:14 +0200

    Unite Link components params API

--- src/components/LinkContainer.jsx
+++ src/components/LinkContainer.jsx
@@ -4,35 +4,28 @@ import React from 'react';
 import { LinkContainer as BSContainer } from 'react-router-bootstrap';
 
 import { reverse } from '../routeTable';
+import { Children } from '../proptypes';
 
-const LinkContainer = ({ children, lang, to, dispatch, routeParams, ...other }) => (
-  <BSContainer
-    to={reverse(lang, to, {
-      ...routeParams,
-    })}
-    {...other}
-  >
+const LinkContainer = ({ children, lang, to, dispatch, params, ...other }) => (
+  <BSContainer to={reverse(lang, to, params)} {...other}>
     {children}
   </BSContainer>
 );
 
 LinkContainer.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.arrayOf(PropTypes.node),
-  ]),
+  children: Children,
   lang: PropTypes.string.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
   dispatch: PropTypes.any,
   // eslint-disable-next-line react/forbid-prop-types
-  routeParams: PropTypes.object,
+  params: PropTypes.object,
   to: PropTypes.string.isRequired,
 };
 
 LinkContainer.defaultProps = {
   children: null,
   dispatch: null,
-  routeParams: null,
+  params: null,
 };
 
 export default LinkContainer;
