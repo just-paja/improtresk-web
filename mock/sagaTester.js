3942ec9c7ae2220059d9621d29e3cb9b382e55f5
Pavel Žák <pavel@berrycloud.cz>
2019-02-27 09:08:07 +0100

    Upgrade redux

--- mock/sagaTester.js
+++ mock/sagaTester.js
@@ -1,20 +1,20 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import SagaTester from 'redux-saga-tester';
+import SagaTester from 'redux-saga-tester'
 
-import { all, fork } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects'
 
-import reducers from '../src/reducers';
+import reducers from '../src/reducers'
 
 export default (initialState) => {
   const sagaTester = new SagaTester({
     reducers,
-    initialState,
-  });
+    initialState
+  })
   sagaTester.runAll = (sagas) => {
-    function* combinedSaga() {
-      yield all(sagas.map(saga => fork(saga)));
+    function * combinedSaga() {
+      yield all(sagas.map(saga => fork(saga)))
     }
-    sagaTester.run(combinedSaga);
-  };
-  return sagaTester;
-};
+    sagaTester.run(combinedSaga)
+  }
+  return sagaTester
+}
