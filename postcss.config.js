42d775b1381df03b51983ea9470e7dad999b4e94
just-paja <just.paja@gmail.com>
2019-03-02 15:12:10 +0100

    Bring back tests with styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-preset-env': {
-      stage: 0,
-      preserve: false
-    }
-  }
+  plugins: [
+    require('postcss-import'),
+    require('postcss-preset-env')
+  ]
 }
