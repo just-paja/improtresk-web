d70899176f917e4eb1c5a7921d21592469b2c641
just-paja <just.paja@gmail.com>
2019-03-02 22:36:19 +0100

    Log error stack

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,16 +1,22 @@
 import winston from 'winston'
 
+const errorStackFormat = winston.format(info => (
+  info instanceof Error
+    ? Object.assign({}, info, { message: info.stack })
+    : info
+))
+
 const format = winston.format.combine(
+  winston.format.splat(),
+  errorStackFormat(),
   winston.format.cli(),
-  winston.format.errors()
+  winston.format.timestamp()
 )
 
 export default winston.createLogger({
   level: 'debug',
   format,
   transports: [
-    new winston.transports.Console({
-      format
-    })
+    new winston.transports.Console()
   ]
 })
