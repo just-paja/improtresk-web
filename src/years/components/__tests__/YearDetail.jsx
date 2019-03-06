56ebb3e1ed77bf8155647e3d4a4f4e24e426d6df
just-paja <just.paja@gmail.com>
2019-03-06 08:13:26 +0100

    Fix YearDetail test

--- src/years/components/__tests__/YearDetail.jsx
+++ src/years/components/__tests__/YearDetail.jsx
@@ -16,7 +16,21 @@ describe('Year Detail component', () => {
         year='2019'
       />
     )
-    expect(comp.find('.year')).toHaveText('Improtřesk 2019')
+    expect(comp.find('h1')).toIncludeText('Improtřesk 2019')
+  })
+
+  it('renders year topic', () => {
+    const comp = shallow(
+      <YearDetail
+        current
+        endDate='2019-05-09'
+        startDate='2019-05-06'
+        startSignupsAt='2019-03-01T00:00:00'
+        topic='Porno je taky improvizace'
+        year='2019'
+      />
+    )
+    expect(comp.find('h1')).toIncludeText('Porno je taky improvizace')
   })
 
   it('renders without signup button when year is not current', () => {
